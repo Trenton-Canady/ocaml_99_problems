2167bc5086dd2bcfd8439a500ee38679f20a6e64
Trenton-Canady <trentoncanady@gmail.com>
2025-09-10 19:11:09 -0400

    gitignore

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,11 @@
-let () = print_endline "Hello, World!"
+let rec last list = 
+    match list with 
+    | [] -> None
+    | [x] -> Some x
+    | _ :: tl -> last tl
+
+let () = 
+    let a = last ["a"; "b"; "c"; "d"] in
+    let final_a = Option.value a ~default:"None" in
+    Printf.printf "Last in list: %s\n" final_a
+
